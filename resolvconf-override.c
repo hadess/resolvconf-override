7b292ef0c1907bd8aaa71e4936a938c5167900d7
Bastien Nocera <hadess@hadess.net>
2019-03-19 13:42:26 +0100

    Add copyright header

--- resolvconf-override.c
+++ resolvconf-override.c
@@ -1,4 +1,21 @@
-// gcc -Wall -g -shared -fPIC -ldl -o libtest.so test.c
+/* -*- Mode: C; indent-tabs-mode: t -*- */
+
+/*
+ * Copyright 2013, 2017 Bastien Nocera
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301  USA.
+ *
+ * Authors: Bastien Nocera <hadess@hadess.net>
+ *
+ */
 
 #include <resolv.h>
 #include <sys/types.h>
